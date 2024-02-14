83f679e3149cfb7b2fb8869ac1e85273d33a3de6
Vaishnavi Kesherwani <vaishnavikesherwani786@gmail.com>
2024-02-14 17:01:51 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.ShadowDom,
+  // inputs: ['IData'],
+})
+export class AppComponent {
+  title = 'Alld';
+  Age = 22;
+  // IData:any
+}
